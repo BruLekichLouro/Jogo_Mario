156cfcae6cd6f1d826a300d3fbda12ace4e9b8fe
BruLekichLouro <bububio@yahoo.com.br>
2023-08-02 16:44:35 -0300

    Começando código para controlar o jogo

---
+++ characters_environment.js
@@ -0,0 +1,9 @@
+//Criando as variáveis dos  elementos:
+var mario, bricks, clouds, mountains, enemyMushrooms, pipes, platforms, coins;
+
+var control={
+    up: "UP_ARROW",
+    left: "LEFT_ARROW",
+    right: "RIGHT_ARROW",
+    revive: 32 //barra de espaço
+}
